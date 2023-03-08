4a3fee221dcc7c06560b4f27b1bdf02624557d15
saleh1312 <profsoft1312@gmail.com>
2023-03-09 01:18:23 +0200

    added

--- src/pages/chat/shapes.js
+++ src/pages/chat/shapes.js
@@ -6,15 +6,21 @@ export function ButtonsMsg(props) {
     const [ele,i,send_message]=props.data
 
     return(
-        <div className="w-100 d-flex flex-row mt-2 pl-2" key={i}>
-          
-            <div className="p-2 d-flex flex-column" style={{borderTopLeftRadius:"20px" , borderTopRightRadius:"20px" ,  borderBottomRightRadius:"20px" , backgroundColor:"#e0e0e0",maxWidth:"80%" }}>
-              <span className='pl-2'>{ele.data[0]?.content}</span>
-                {
-                    ele.data.slice(1,ele.data.length).map((btn,j)=>{
-                        return(<button onClick={()=>{send_message(false,ele.data[j+1].id,ele.data[j+1].content,btn)}} className='btn btn-secondary m-2' key={j} style={{minWidth:"110px" }}>{btn.content}</button>)
-                    })
-                }
+        <div className="w-100 d-flex flex-column justify-content-center align-items-center mt-2 pl-2" key={i}>
+            
+            <div className='w-100 d-flex flex-row justify-content-start'>
+            <span className='pl-2'>{ele.data[0]?.content}</span>
+            </div>
+            <div className="p-2 d-flex flex-column" style={{borderTopLeftRadius:"20px" , borderTopRightRadius:"20px" ,  borderBottomRightRadius:"20px" , backgroundColor:"transparent",maxWidth:"80%" }}>
+              
+              <div className='d-flex flex-column ' >
+                  {
+                      ele.data.slice(1,ele.data.length).map((btn,j)=>{
+                          return(<button onClick={()=>{send_message(false,ele.data[j+1].id,ele.data[j+1].content,btn)}} className='btn btn-secondary m-2' key={j} style={{minWidth:"110px" }}>{btn.content}</button>)
+                      })
+                  }
+              </div>
+                
 
                 
 
@@ -50,7 +56,7 @@ export function RepliedButton(props) {
   if(ele.sent_by_customer===true){
       return(
         <div className="w-100 d-flex flex-row-reverse mt-2 pr-2" key={i}>
-          <div className="bg-warning p-2 d-flex flex-row justify-content-center align-items-center" style={{maxWidth:"80%",minWidth:"20%" , borderTopLeftRadius:"20px" , borderTopRightRadius:"20px" ,  borderBottomLeftRadius:"20px" }}>
+          <div className="p-2 d-flex flex-row justify-content-center align-items-center" style={{maxWidth:"80%",minWidth:"20%" , borderTopLeftRadius:"20px" , borderTopRightRadius:"20px" ,  borderBottomLeftRadius:"20px" }}>
             <span style={{wordWrap: "break-word"}}>{ele.data.content}</span>
 
           </div>
